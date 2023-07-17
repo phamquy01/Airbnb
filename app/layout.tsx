d6fcf053af553b6bc7cc163ddd7841081f08aa0f
phamquy01 <quypham07102001@gmail.com>
2023-07-17 22:46:41 +0700

    airbnb-login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { Nunito } from 'next/font/google';
-import Navbar from './components/navbar/Navbar';
-const inter = Inter({ subsets: ['latin'] });
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
+  title: "Airbnb",
+  description: "Airbnb clone",
 };
 
 const font = Nunito({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 export default function RootLayout({
   children,
@@ -21,7 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Modal title="Login Modal" isOpen />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
