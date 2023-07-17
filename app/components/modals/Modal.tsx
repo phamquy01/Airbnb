e2371f47deb0a8c4ead7817118649f039554a2d8
phamquy01 <quypham07102001@gmail.com>
2023-07-17 22:53:43 +0700

    airbnb-login

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -86,7 +86,7 @@ const Modal: React.FC<ModalProps> = ({
               {/* FOOTER */}
               <div className="flex flex-col gap-2 p-6">
                 <div className="flex -flex-row items-center gap-4 w-full">
-                  <Button label="Login" />
+                  {/* <Button label="Login" /> */}
                 </div>
               </div>
             </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal title="Login Modal" isOpen />
+          {/* <Modal title="Login Modal" isOpen /> */}
           <Navbar />
         </ClientOnly>
         {children}
