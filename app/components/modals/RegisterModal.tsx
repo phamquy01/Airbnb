4c25376a6062b5cb828c27956ae6528503308cd2
phamquy01 <quypham07102001@gmail.com>
2023-07-19 23:41:28 +0700

    airbnb: register modal

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import axios from "axios";
+import React, { useState, useCallback } from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import Modal from "./Modal";
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+    />
+  );
+};
+
+export default RegisterModal;
